10766fab83b90a1d490153310de6c346962934fc
helloitjase <jason.sl.chen@gmail.com>
2019-07-26 21:06:28 -0700

    able to populate rooms, need to work on adding rooms to server

--- client/scripts/roomsView.js
+++ client/scripts/roomsView.js
@@ -10,19 +10,21 @@ var RoomsView = {
 
   render: function() {
     //add rooms to dropdown list already in memory
-    this.$select.append('<option value="1">New Room</option><option value="2">Fun Times</option><option value="3">Not Fun Times</option><option value="4">Mediocre Times</option>');
+    // this.$select.append('<option value="1">New Room</option><option value="2">Fun Times</option><option value="3">Not Fun Times</option><option value="4">Mediocre Times</option>');
 
     //add a text field and make new room button to container
-    // $('#rooms').append('<input type="text" name="message" id="message"/>');
+    $('#rooms').append('<input type="text" name="message" id="add-room"/>');
   },
 
-  renderRoom: function() {
-    //for the new room option
-      //create a new input box and button
-    //on make new room click
-      //grab input and add to $select
-    //grab text from input box
-    //create a new room based on text
+  renderRoom: function(roomName) {
+    //grab input from add room text field
+     roomName = $('#add-room').val();
+    //add an index to track option value
+    let index = 1;
+    //add new room input to $select dropdown
+    $('#rooms select').append(`<option value="${index}">${roomName}</option>`);
+    //increase index
+    index++;
   },
 
   toggleRoom: function() {
